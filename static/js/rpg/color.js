d3d970848110ad66da0f2f807f46cefdba8ec761
MestourBS <BashirSalaheddine.Mestour@formation.orif.ch>
2022-01-18 10:06:47 +0100

    Add alpha (transparency) for colors Move bounds checking to its own function Slightly change mix

--- static/js/rpg/color.js
+++ static/js/rpg/color.js
@@ -1,4 +1,19 @@
+const regex_rgb = /^rgb\((\d+(?:\.\d+)?), *(\d+(?:\.\d+)?), *(\d+(?:\.\d+)?)\)$/i;
+const regex_rgba = /^rgba\((\d+(?:\.\d+)?), *(\d+(?:\.\d+)?), *(\d+(?:\.\d+)?)\, *(\d+(?:\.\d+)?)\)$/i;
+const regex_hsl = /^hsl\((\d+(?:\.\d+)?), *(\d+(?:\.\d+)?)%, *(\d+(?:\.\d+)?)%\)$/i
+const regex_hsla = /^hsla\((\d+(?:\.\d+)?), *(\d+(?:\.\d+)?)%, *(\d+(?:\.\d+)?)%, *(\d+(?:\.\d+)?)%\)$/i;
+
 export class Color {
+    /**
+     * Forces a number to be between 0 and 255 (inclusives)
+     *
+     * @param {number} number
+     * @returns {number}
+     */
+    static #kib(number) {
+        return Math.round(Math.max(0, Math.min(255, number)));
+    }
+
     /**
      * Converts a #rrggbbaa color into a Color object
      *
@@ -11,12 +26,15 @@ export class Color {
 
         if (hex.length <= 4) hex = hex.split('').map(s => s.repeat(2)).join('');
 
-        let rgb = [0, 0, 0];
-        for (let i = 0; i < 3; i++) rgb[i] = parseInt(hex.slice(i * 2, i * 2 + 2), 16);
+        let rgba = [0, 0, 0, 255];
+        let max_i = 3 + (hex.length == 8);
+        for (let i = 0; i < max_i; i++) {
+            rgba[i] = parseInt(hex.slice(i * 2, i * 2 + 2), 16);
+        }
 
-        let [red, green, blue] = rgb;
+        let [red, green, blue, alpha] = rgba;
 
-        return new Color(red, green, blue);
+        return new Color(red, green, blue, alpha);
     }
     /**
      * Converts red, green, blue into a Color object
@@ -26,18 +44,17 @@ export class Color {
      * @param {number} red
      * @param {number} green
      * @param {number} blue
+     * @param {number} [alpha]
      * @returns {Color}
      */
-    static from_rgb(red, green, blue) {
-        if (isNaN(red)) throw new TypeError(`${red} is not a number`);
-        if (isNaN(green)) throw new TypeError(`${green} is not a number`);
-        if (isNaN(blue)) throw new TypeError(`${blue} is not a number`);
+    static from_rgb(red, green, blue, alpha=255) {
+        if (isNaN(red)) throw new TypeError(`red (${red}) is not a number`);
+        if (isNaN(green)) throw new TypeError(`green (${green}) is not a number`);
+        if (isNaN(blue)) throw new TypeError(`blue (${blue}) is not a number`);
+        if (isNaN(alpha)) throw new TypeError(`alpha (${alpha}) is not a number`);
 
-        red = Math.min(255, Math.max(0, red));
-        green = Math.min(255, Math.max(0, green));
-        blue = Math.min(255, Math.max(0, blue));
-
-        return new Color(red, green, blue);
+        const kib = this.#kib;
+        return new Color(kib(red), kib(green), kib(blue), kib(alpha));
     }
     /**
      * Converts a `rgb(red, green, blue)` or a `rgba(red, green, blue, alpha)` into a Color object
@@ -46,10 +63,14 @@ export class Color {
      * @returns {Color}
      */
     static from_css_rgb(cssrgb) {
-        if (!cssrgb.match(/^rgba?\((\d+), ?(\d+), ?(\d+)/i)) throw new TypeError(`${cssrgb} is not in the format 'rgb(<red>, <green>, <blue>)' or 'rgba(<red>, <green>, <blue>, <alpha>)`);
+        let has_alpha;
+        if (cssrgb.match(regex_rgb)) has_alpha = false;
+        else if (cssrgb.match(regex_rgba)) has_alpha = true;
+        else throw new TypeError(`${cssrgb} is not in the format 'rgb(<red>, <green>, <blue>)' or 'rgba(<red>, <green>, <blue>, <alpha>)`);
 
-        let [,red, green, blue] = cssrgb.match(/^rgba?\((\d+), ?(\d+), ?(\d+)/i);
-        return new Color(+red, +green, +blue);
+        let [,red, green, blue, alpha='255'] = cssrgb.match(has_alpha ? regex_rgba : regex_rgb);
+        const kib = this.#kib;
+        return new Color(kib(red), kib(green), kib(blue), kib(alpha));
     }
     /**
      * Converts a `hsl(hue, saturation, lightness)` into a Color object
@@ -58,9 +79,14 @@ export class Color {
      * @returns {Color}
      */
     static from_css_hsl(csshsl) {
-        if (!csshsl.match(/^hsla?\((\d+), ?(\d+)%, ?(\d+)/i)) throw new TypeError(`${csshsl} is not in the format 'hsl(<hue>, <saturation>, <lightness>)' or 'hsla(<hue>, <saturation>, <lightness>, <alpha>)'`);
+        let has_alpha;
+        if (csshsl.match(regex_hsl)) has_alpha = false;
+        else if (csshsl.match(regex_hsla)) has_alpha = true;
+        else throw new TypeError(`${csshsl} is not in the format 'hsl(<hue>, <saturation>, <lightness>)' or 'hsla(<hue>, <saturation>, <lightness>, <alpha>)'`);
 
-        let [,hue, saturation, lightness] = csshsl.match(/^hsla?\((\d+), ?(\d+)%, ?(\d+)/i);
+        let [,hue, saturation, lightness, alpha=100] = csshsl.match(has_alpha ? regex_hsla : regex_hsl);
+
+        alpha = alpha / 100 * 255;
 
         // @see https://stackoverflow.com/a/44134328
         lightness /= 100;
@@ -76,28 +102,27 @@ export class Color {
         let green = f(8);
         let blue = f(4);
 
-        return new Color(red, green, blue);
+        const kib = this.#kib;
+        return new Color(kib(red), kib(green), kib(blue), kib(alpha));
     }
     /**
      * Converts an object with the red, green, and blue properties into a Color object
      *
-     * @param {{red: number, green: number, blue: number}} color
+     * @param {{red: number, green: number, blue: number, alpha?: number}} color
      */
     static from_object(color) {
         if (!('red' in color)) throw new TypeError(`${color} has no red property`);
         if (!('green' in color)) throw new TypeError(`${color} has no green property`);
         if (!('blue' in color)) throw new TypeError(`${color} has no blue property`);
-        if (isNaN(color.red)) throw new TypeError(`${color.red} is NaN`);
-        if (isNaN(color.green)) throw new TypeError(`${color.green} is NaN`);
-        if (isNaN(color.blue)) throw new TypeError(`${color.blue} is NaN`);
+        if (isNaN(color.red)) throw new TypeError(`color's red property (${color.red}) is NaN`);
+        if (isNaN(color.green)) throw new TypeError(`color's green property (${color.green}) is NaN`);
+        if (isNaN(color.blue)) throw new TypeError(`color's blue property (${color.blue}) is NaN`);
+        if ('alpha' in color && isNaN(color.alpha)) throw new TypeError(`color's alpha property (${color.alpha}) is NaN`);
 
-        let {red, green, blue} = color;
+        let {red, green, blue, alpha=255} = color;
 
-        red = Math.min(255, Math.max(0, red));
-        green = Math.min(255, Math.max(0, green));
-        blue = Math.min(255, Math.max(0, blue));
-
-        return new Color(red, green, blue);
+        const kib = this.#kib;
+        return new Color(kib(red), kib(green), kib(blue), kib(alpha));
     }
     /**
      * Converts an html color name into a Color object
@@ -119,8 +144,9 @@ export class Color {
         let red = Math.floor(Math.random() * 255);
         let green = Math.floor(Math.random() * 255);
         let blue = Math.floor(Math.random() * 255);
+        let alpha = Math.floor(Math.random() * 255);
 
-        return new Color(red, green, blue);
+        return new Color(red, green, blue, alpha);
     }
 
     /** @type {number} */
@@ -129,6 +155,8 @@ export class Color {
     #green;
     /** @type {number} */
     #blue;
+    /** @type {number} */
+    #alpha;
     /** @type {string|false} */
     #string_hex = false;
     /** @type {string|false} */
@@ -137,38 +165,54 @@ export class Color {
     #string_hsl = false;
 
     /**
-     * @param {Color|string|number|{red: number, green: number, blue: number}} red
+     * @param {Color|string|number|{red: number, green: number, blue: number, alpha?: number}} red
      * @param {number} [green]
      * @param {number} [blue]
+     * @param {number} [alpha]
      */
-    constructor(red, green, blue) {
-        if (typeof red == 'object' && 'red' in red && 'green' in red && 'blue' in red) ({red, green, blue} = red);
-        else if (typeof red == 'string') ({red, green, blue} = Color.from_hex(red));
-        else if (typeof red == 'number' && typeof green == 'number' && typeof blue == 'number');
-        else throw new TypeError(`Invalid color arguments: ${red.toString()},${green.toString()},${blue.toString()}`);
+    constructor(red, green, blue, alpha=255) {
+        if (typeof red == 'object') {
+            if (isNaN(red?.red) || isNaN(red?.green) || isNaN(red?.blue) || ('alpha' in red && isNaN(red.alpha)))
+            ({red, green, blue, alpha=alpha} = red);
+        } else if (typeof red == 'string') ({red, green, blue, alpha=alpha} = Color.from_hex(red));
+        else if (!isNaN(red) && !isNaN(green) && !isNaN(blue));
+        else throw new TypeError(`Invalid color arguments: ${red},${green},${blue}`);
+        if (isNaN(alpha)) throw new TypeError(`Invalid color alpha: ${alpha}`);
 
-        this.#red = red;
-        this.#green = green;
-        this.#blue = blue;
+        const kib = Color.#kib;
+
+        this.#red = kib(red);
+        this.#green = kib(green);
+        this.#blue = kib(blue);
+        this.#alpha = kib(alpha);
     }
 
     /**
-     * @param {'hex'|'rgb'|'hsl'} format
+     * @param {'hex'|'rgb'|'hsl'} [format]
+     * @param {boolean|'auto'} [alpha]
      */
-    toString(format='hex') {
+    toString(format='hex', alpha='auto') {
         let string;
+        if (alpha == 'auto') alpha = this.#alpha < 255;
         switch (format) {
             default:
                 console.error(`Unknown mode ${format}, defaulting to rgb`);
             case 'hex':
                 if (!this.#string_hex) {
                     this.#string_hex = `#${this.#red.toString(16).padStart(2,'0')}${this.#green.toString(16).padStart(2,'0')}${this.#blue.toString(16).padStart(2,'0')}`;
+                    if (alpha) this.#string_hex += this.#alpha.toString(16).padStart(2,'0');
                 }
                 string = this.#string_hex;
                 break;
             case 'rgb':
                 if (!this.#string_rgb) {
-                    this.#string_rgb = `rgb(${this.#red}, ${this.#green}, ${this.#blue})`;
+                    let rgb = 'rgb';
+                    let colors = [this.#red, this.#green, this.#blue];
+                    if (alpha) {
+                        rgb += 'a';
+                        colors.push(this.#alpha);
+                    }
+                    this.#string_rgb = `${rgb}(${colors.join(', ')})`;
                 }
                 string = this.#string_rgb;
                 break;
@@ -202,7 +246,13 @@ export class Color {
                     s = +(s * 100).toFixed(1);
                     l = +(l * 100).toFixed(1);
 
-                    this.#string_hsl = `hsl(${h}, ${s}%, ${l}%)`;
+                    let hsl = 'hsl';
+                    let values = [h.toString(), `${s}%`, `${l}%`];
+                    if (alpha) {
+                        hsl += 'a';
+                        values.push(`${this.#alpha / 255 * 100}%`)
+                    }
+                    this.#string_hsl = `${hsl}(${values.join(', ')})`;
                 }
                 string = this.#string_hsl;
                 break;
@@ -215,7 +265,8 @@ export class Color {
     get red() { return this.#red; }
     set red(red) {
         if (!isNaN(red)) {
-            this.#red = Math.min(255, Math.max(0, red));
+            const kib = Color.#kib;
+            this.#red = kib(red);
             this.#string_hex = false;
             this.#string_rgb = false;
             this.#string_hsl = false;
@@ -224,7 +275,8 @@ export class Color {
     get green() { return this.#green; }
     set green(green) {
         if (!isNaN(green)) {
-            this.#green = Math.min(255, Math.max(0, green));
+            const kib = Color.#kib;
+            this.#green = kib(green);
             this.#string_hex = false;
             this.#string_rgb = false;
             this.#string_hsl = false;
@@ -233,7 +285,18 @@ export class Color {
     get blue() { return this.#blue; }
     set blue(blue) {
         if (!isNaN(blue)) {
-            this.#blue = Math.min(255, Math.max(0, blue));
+            const kib = Color.#kib;
+            this.#blue = kib(blue);
+            this.#string_hex = false;
+            this.#string_rgb = false;
+            this.#string_hsl = false;
+        }
+    }
+    get alpha() { return this.#alpha; }
+    set alpha(alpha) {
+        if (!isNaN(alpha)) {
+            const kib = Color.#kib;
+            this.#alpha = kib(alpha);
             this.#string_hex = false;
             this.#string_rgb = false;
             this.#string_hsl = false;
@@ -248,7 +311,7 @@ export class Color {
     grayscale() {
         let gray = Math.round((this.#red + this.#green + this.#blue) / 3);
 
-        return new Color(gray, gray, gray);
+        return new Color(gray, gray, gray, this.#alpha);
     }
     /**
      * Returns an inverted version of the color
@@ -260,7 +323,7 @@ export class Color {
         let green = 255 - this.#green;
         let blue = 255 - this.#blue;
 
-        return new Color(red, green, blue);
+        return new Color(red, green, blue, this.#alpha);
     }
     /**
      * Clones the color
@@ -287,7 +350,7 @@ export class Color {
         green *= percent;
         blue *= percent;
 
-        return new Color(Math.floor(red), Math.floor(green), Math.floor(blue));
+        return new Color(Math.floor(red), Math.floor(green), Math.floor(blue), this.#alpha);
     }
     /**
      * Makes a brighter version of this color
@@ -305,7 +368,7 @@ export class Color {
         green = 255 - (255 - green) * percent;
         blue = 255 - (255 - blue) * percent;
 
-        return new Color(Math.floor(red), Math.floor(green), Math.floor(blue));
+        return new Color(Math.floor(red), Math.floor(green), Math.floor(blue), this.#alpha);
     }
     /**
      * Mixes 2 colors
@@ -315,62 +378,39 @@ export class Color {
      * @returns {Color}
      */
     mix(color, mode='average') {
-        let red = 0;
-        let green = 0;
-        let blue = 0;
+        const ops = {
+            'average': (a, b) => (a + b) / 2,
+            'max': Math.max,
+            'min': Math.min,
+            'add': (a, b) => a + b,
+            'substract': (a, b) => a - b,
+            'multiply': (a, b) => a * b,
+            'xor': (a, b) => a ^ b,
+            'and': (a, b) => a & b,
+            'or': (a, b) => a | b,
+        };
 
-        switch (mode) {
-            case 'average':
-                red = (this.#red + color.#red) / 2;
-                green = (this.#green + color.#green) / 2;
-                blue = (this.#blue + color.#blue) / 2;
-                break;
-            case 'max':
-                red = Math.max(this.#red, color.#red);
-                green = Math.max(this.#green, color.#green);
-                blue = Math.max(this.#blue, color.#blue);
-                break;
-            case 'min':
-                red = Math.min(this.#red, color.#red);
-                green = Math.min(this.#green, color.#green);
-                blue = Math.min(this.#blue, color.#blue);
-                break;
-            case 'add':
-                red = Math.min(255, this.#red + color.#red);
-                green = Math.min(255, this.#green + color.#green);
-                blue = Math.min(255, this.#blue + color.#blue);
-                break;
-            case 'substract':
-                red = Math.max(0, this.#red - color.#red);
-                green = Math.max(0, this.#green - color.#green);
-                blue = Math.max(0, this.#blue - color.#blue);
-                break;
-            case 'multiply':
-                red = Math.min(255, this.#red * color.#red);
-                green = Math.min(255, this.#green * color.#green);
-                blue = Math.min(255, this.#blue * color.#blue);
-                break;
-            case 'xor':
-                red = this.#red ^ color.#red;
-                green = this.#green ^ color.#green;
-                blue = this.#blue ^ color.#blue;
-                break;
-            case 'and':
-                red = this.#red & color.#red;
-                green = this.#green & color.#green;
-                blue = this.#blue & color.#blue;
-                break;
-            case 'or':
-                red = this.#red | color.#red;
-                green = this.#green | color.#green;
-                blue = this.#blue | color.#blue;
-                break;
-            default:
-                console.error(`Unknown color mix mode ${mode}`);
-                return null;
+        if (!(mode in ops)) {
+            console.error(`Unknown color mix mode ${mode}`);
+            return null;
         }
 
-        return new Color(red, green, blue);
+        let result = {
+            red: 0,
+            green: 0,
+            blue: 0,
+            alpha: 0,
+        };
+
+        /** @type {(a: number, b: number) => number} */
+        let op = ops[mode];
+        const kib = Color.#kib;
+
+        for (let prop of Object.keys(result)) {
+            result[prop] = kib(op(this[prop], color[prop]));
+        }
+
+        return new Color(result);
     }
 }
 export default Color;
