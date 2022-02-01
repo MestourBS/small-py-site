df2c67c55d04596ead47181b88a6edd26a36baef
MestourBS <BashirSalaheddine.Mestour@formation.orif.ch>
2022-02-01 13:16:26 +0100

    Add list of games

--- Controllers/ControllerGame.py
+++ Controllers/ControllerGame.py
@@ -1,6 +1,17 @@
-from flask import Blueprint, render_template, Response
+from gettext import gettext
+from flask import Blueprint, render_template, url_for
 
 routeGame = Blueprint('games', __name__, template_folder='templates')
+
+@routeGame.route('/')
+def games():
+    links = [
+        {'name': 'games_blockus', 'link': url_for('games.blockus_game')},
+        #{'name': 'games_idle', 'link': url_for('games.idle_game')},
+        #{'name': 'games_rpg', 'link': url_for('games.rpg_game')},
+        #{'name': 'games_cidle', 'link': url_for('games.cidle_game')},
+    ]
+    return render_template('games/list.html', links=links)
 
 @routeGame.route('/blockus')
 def blockus_game():
